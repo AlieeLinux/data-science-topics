a8305092f599a09b24e1bf0f275d0aee28a474f0
troy600 <bazartroy531@gmail.com>
2025-01-28 15:13:39 +0800

    moves to folder to make it cleener

---
+++ String-Examples/SimpleString.java
@@ -0,0 +1,12 @@
+public class SimpleString {
+    public static void main(String args[]) {
+        String any = "put anything you want here";
+
+        //print the string out
+        System.out.println(any); //<--- call the string
+        
+
+        //the output is called any String
+//        System.out.println("seemzie");
+    }
+}
