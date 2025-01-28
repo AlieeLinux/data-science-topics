b4dba84c0b3dad5c326fd4ac3136724e5b1abb4b
troy600 <bazartroy531@gmail.com>
2025-01-28 14:03:40 +0800

    Eeeeeeeeeeeeeeeeeee

---
+++ Ints.java
@@ -0,0 +1,12 @@
+//this is a comment(has //)
+
+public class Ints {
+    public static void main(String args[]) {
+        //assume a and b is given
+        int a=2, b=1;
+
+        int c = (a*2+2*a*b-4*(1+2))/2;
+        
+        System.out.println(c);
+    }
+}
