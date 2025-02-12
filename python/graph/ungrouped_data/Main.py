69082aa4462abe9f680e841fe35ba1cfc1694350
troy600 <bazartroy531@gmail.com>
2025-02-12 20:53:12 +0800

    Statistic proof of concepts

--- python/graph/ungrouped_data/Main.py
+++ python/graph/ungrouped_data/Main.py
@@ -1,8 +1,8 @@
 import Simpletable
 import numpy as np
 
-x = np.array([6, 9, 12, 15, 18, 21, 24])
-y = np.array([2, 4, 14, 9, 6, 3, 2])
+x = np.array([15.5, 25.5, 35.5, 45.5, 55.5, 65.5, 75.5])
+y = np.array([5, 7, 12, 22, 8, 4, 2])
 
-cumfreq = Simpletable.unGroupData(x, y)
-Simpletable.GraphOgive(x, cumfreq)
+Simpletable.unGroupData(x, y)
+#Simpletable.GraphOgive(x, cumfreq)
