93b01d7eef68e0879134d391eec778c6be4feab2
troy600 <bazartroy531@gmail.com>
2025-02-14 10:38:45 +0800

    Vision And Mission of ISU on Github

---
+++ lesson31/VisionMissionOfIsu.java
@@ -0,0 +1,9 @@
+class VisionMissionOfIsu {
+    public static void main(String args[]) {
+        String VisionMission = "Vision \n\tA leading Research University in the ASEAN Region. \n\n\nMission \n\t The Isabela State University is committed to develop globally competitive human, technological resources and services through quality instruction, innovative research, responsive community engagement and viable resource management programs for inclusive growth and sustainable development.";
+
+        System.out.println(VisionMission);
+
+        System.out.println();
+    } 
+}
