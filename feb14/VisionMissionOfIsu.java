17e9e1eeaba7c0413594dc32ae3540226ba6b4c2
troy600 <bazartroy531@gmail.com>
2025-02-14 21:41:54 +0800

    Removed the extra System println

--- feb14/VisionMissionOfIsu.java
+++ feb14/VisionMissionOfIsu.java
@@ -3,7 +3,5 @@ class VisionMissionOfIsu {
         String VisionMission = "Vision \n\tA leading Research University in the ASEAN Region. \n\n\nMission \n\t The Isabela State University is committed to develop globally competitive human, technological resources and services through quality instruction, innovative research, responsive community engagement and viable resource management programs for inclusive growth and sustainable development.";
 
         System.out.println(VisionMission);
-
-        System.out.println();
     } 
 }
