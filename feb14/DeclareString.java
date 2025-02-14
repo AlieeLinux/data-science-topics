ec157bd2e25ec80cac4e9fab5a62d1c3800a904f
troy600 <bazartroy531@gmail.com>
2025-02-14 11:59:31 +0800

    Activity submition

---
+++ feb14/DeclareString.java
@@ -0,0 +1,9 @@
+class DeclareString {
+    public static void main(String args[]) {
+        String isabela = "ISABELA ", state="STATE ", university = "UNIVERSITY";
+
+        System.out.println(isabela + state + university);
+
+
+    }
+}
---
+++ feb14/VisionMissionOfIsuUppercase.java
@@ -0,0 +1,7 @@
+class VisionMissionOfIsuUppercase {
+    public static void main(String args[]) {
+        String VisionMission = "Vision \n\tA leading Research University in the ASEAN Region. \n\n\nMission \n\t The Isabela State University is committed to develop globally competitive human, technological resources and services through quality instruction, innovative research, responsive community engagement and viable resource management programs for inclusive growth and sustainable development.";
+
+        System.out.println(VisionMission + "\n\n\n\n\n" + VisionMission.toUpperCase() + "\n\n\n\n\n" + VisionMission.toLowerCase());
+    } 
+}
