1a196e4c8572ad759fe67e0a094290486259aa2e
troy600 <bazartroy531@gmail.com>
2025-02-14 11:32:04 +0800

    Vssccc

---
+++ lesson31/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+class myclass {
+    public:
+        myclass() {
+            std::cout << 9*8/9/9/9+9+9;
+        }
+};
+
+
+
+int main(void) {
+    myclass();
+}
---
+++ lesson31/Timer.java
@@ -0,0 +1,11 @@
+class Timer {
+    public static void main(String args[]){
+        int secand, second;
+        secand = 10;
+        second = 20;
+        while (second <= 999) {
+            second++;
+            System.out.println(second);
+        }
+    }
+}
