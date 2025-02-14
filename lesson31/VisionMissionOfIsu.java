a25fefdcb8c62a4d86cf6084527a3040b3f771f9
troy600 <bazartroy531@gmail.com>
2025-02-14 21:38:32 +0800

    file moved

--- lesson31/VisionMissionOfIsu.java
+++
@@ -1,9 +0,0 @@
-class VisionMissionOfIsu {
-    public static void main(String args[]) {
-        String VisionMission = "Vision \n\tA leading Research University in the ASEAN Region. \n\n\nMission \n\t The Isabela State University is committed to develop globally competitive human, technological resources and services through quality instruction, innovative research, responsive community engagement and viable resource management programs for inclusive growth and sustainable development.";
-
-        System.out.println(VisionMission);
-
-        System.out.println();
-    } 
-}
