7a16cea6c09ab881703a1cb9ffc9667223e3c8aa
troy600 <bazartroy531@gmail.com>
2025-03-03 08:05:48 +0800

    File andllings

---
+++ python/file_handling/Read.py
@@ -0,0 +1,9 @@
+#I eedit nito yung file.txt
+import Write
+import os
+
+print(os.path.isfile("hello.txt"))
+
+x = open("hello.txt", "w")
+
+x.write("Deddi ams")
---
+++ python/file_handling/Write.py
@@ -0,0 +1,4 @@
+#Itong File na eto ay gagawa siya ng file na may pangalan na hello.txt
+
+def main():
+    open("hello.txt", "x")
