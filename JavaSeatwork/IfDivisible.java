2f569ac6d9f9b5ee85288e119b8bb1002de33206
troy600 <bazartroy531@gmail.com>
2025-03-05 11:51:53 +0800

    Divisible by 5

---
+++ JavaSeatwork/IfDivisible.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class IfDivisible {
+    
+    public static boolean IfDivisibleByFive(int x) {
+        if (x % 5 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number\n>>");
+
+        int number = input.nextInt();
+        boolean IfDivisible = IfDivisibleByFive(number);
+
+        if (IfDivisible) {
+            System.out.println("Number: " + number + " is Divisible by 5.");
+        } else {
+            System.out.println("Number: " + number + " is not Divisible by 5.");
+        }
+        input.close();
+    }
+} 
